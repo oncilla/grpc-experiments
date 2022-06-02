b9c03d37d6dc82c99bf6d8bd85c9525c639e1735
Dominik Roos <roos@anapaya.net>
2022-06-02 21:01:27 +0200

    http3: use connect-go and quic-go (#1)

---
+++ http3/README.md
@@ -0,0 +1,10 @@
+# HTTP3
+
+Run server:
+
+    go run ./cmd/server/main.go
+
+Run client:
+
+    go run ./cmd/client/main.go
+    2022/06/02 20:58:31 Hello, Jane!
---
+++ http3/cmd/client/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"log"
+	"net/http"
+
+	"github.com/lucas-clemente/quic-go/http3"
+	greetv1 "github.com/oncilla/grpc-experiments/http3/gen/greet/v1"
+	"github.com/oncilla/grpc-experiments/http3/gen/greet/v1/greetv1connect"
+
+	"github.com/bufbuild/connect-go"
+)
+
+func main() {
+	client := greetv1connect.NewGreetServiceClient(
+		&http.Client{
+			Transport: &http3.RoundTripper{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+		},
+		"https://localhost:8080",
+	)
+	res, err := client.Greet(
+		context.Background(),
+		connect.NewRequest(&greetv1.GreetRequest{Name: "Jane"}),
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(res.Msg.Greeting)
+}
